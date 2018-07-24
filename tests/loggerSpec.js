d2743b7bbdae76e2ceb6a901400b3f6e020f53f4
Yehuda Sabag <yehudasabag@gmail.com>
2018-07-24 10:51:49 +0300

    add ability to add 'middleware' like function to the error and logs

--- tests/loggerSpec.js
+++ tests/loggerSpec.js
@@ -55,6 +55,79 @@ describe('logger tests', function () {
             });
         });
     });
+
+    describe('add/clear LogMiddleware', function () {
+        context('when no previous middleware exist', function () {
+            it('add should add one and call it upon a call to the log function', function () {
+                const winston = logger.init();
+                const winstonError = sinon.spy(winston, 'error');
+                const winstonWarn = sinon.spy(winston, 'warn');
+                const log = logger.getLogger('m');
+                let errMiddleware = sinon.spy();
+                log.addLogMiddleware('error', errMiddleware);
+
+                log.error('msg');
+                assert.isTrue(winstonError.calledOnce, 'winston error method not called');
+                assert.isTrue(winstonError.calledWith('msg', {moduleName: 'm'}),
+                    'winston params not expected');
+                assert.isTrue(errMiddleware.calledOnce, 'error middleware function was not called');
+
+                let warnMiddleware = sinon.spy();
+                log.addLogMiddleware('warn', warnMiddleware);
+                log.warn('warn');
+                assert.isTrue(winstonWarn.calledOnce, 'winston warn method not called');
+                assert.isTrue(winstonWarn.calledWith('warn', {moduleName: 'm'}),
+                    'winston params not expected');
+                assert.isTrue(warnMiddleware.calledOnce, 'warn middleware function was not called');
+
+            });
+
+            it('clear should clear existing middleware', function () {
+                const winstonError = sinon.spy(logger.init(), 'error');
+                const log = logger.getLogger('m');
+                let errMiddleware = sinon.spy();
+                log.addLogMiddleware('error', errMiddleware);
+
+                log.error('msg');
+                assert.isTrue(winstonError.calledOnce, 'winston info method not called');
+                assert.isTrue(winstonError.calledWith('msg', {moduleName: 'm'}),
+                    'winston params not expected');
+                assert.isTrue(errMiddleware.calledOnce, 'the middleware function was not called');
+                errMiddleware.resetHistory();
+
+                log.clearLogMiddleware('error');
+                log.error('msg2');
+                assert.isTrue(winstonError.calledTwice, 'winston info method not called');
+                assert.isTrue(winstonError.calledWith('msg2', {moduleName: 'm'}),
+                    'winston params not expected');
+                assert.isTrue(errMiddleware.notCalled, 'the middleware function was called after clear');
+            });
+
+            it('add should throw if the level is not warn or error', function () {
+                const log = logger.getLogger('m');
+                let addInfoMiddleware = () => {
+                    log.addLogMiddleware('info', ()=>{});
+                };
+                assert.throws(addInfoMiddleware);
+            });
+
+        });
+        context('when previous middleware exist', function () {
+            it('add should throw exception', function () {
+                const winston = logger.init();
+                const winstonError = sinon.spy(winston, 'error');
+                const log = logger.getLogger('m');
+                let errMiddleware = sinon.spy();
+                log.addLogMiddleware('error', errMiddleware);
+                let addAnother = () => {
+                    log.addLogMiddleware('error', sinon.spy());
+                };
+                assert.throws(addAnother);
+
+            });
+        })
+
+    });
 });
 
 
